bbb9f0337b6a4a085a661d4bb1484e703ad9abf0
m1ma0314 <m1ma@ucsd.edu>
2025-09-11 00:12:41 -0400

    Initial commit: DBS segmentation module

---
+++ DBS/DBS.py
@@ -0,0 +1,365 @@
+import logging
+import os
+from typing import Annotated, Optional
+
+import vtk
+
+import slicer
+from slicer.i18n import tr as _
+from slicer.i18n import translate
+from slicer.ScriptedLoadableModule import *
+from slicer.util import VTKObservationMixin
+from slicer.parameterNodeWrapper import (
+    parameterNodeWrapper,
+    WithinRange,
+)
+
+from slicer import vtkMRMLScalarVolumeNode
+
+
+#
+# DBS
+#
+
+
+class DBS(ScriptedLoadableModule):
+    """Uses ScriptedLoadableModule base class, available at:
+    https://github.com/Slicer/Slicer/blob/main/Base/Python/slicer/ScriptedLoadableModule.py
+    """
+
+    def __init__(self, parent):
+        ScriptedLoadableModule.__init__(self, parent)
+        self.parent.title = _("Fiducial Automatic Segmentation in Deep Brain Stimulation)  # TODO: make this more human readable by adding spaces
+        # TODO: set categories (folders where the module shows up in the module selector)
+        self.parent.categories = [translate("qSlicerAbstractCoreModule", "Examples")]
+        self.parent.dependencies = []  # TODO: add here list of module names that this module requires
+        self.parent.contributors = ["Mijia Ma, Pedro Moreira (Brigham and Women's Hospital)"]  # TODO: replace with "Firstname Lastname (Organization)"
+        # TODO: update with short description of the module and a link to online module documentation
+        # _() function marks text as translatable to other languages
+        self.parent.helpText = _("""
+This is an example of scripted loadable module bundled in an extension.
+See more information in <a href="https://github.com/organization/projectname#DBS">module documentation</a>.
+""")
+        self.parent.helpText = _("""
+This module provides a fully automatic pipeline for tube segmentation from 3D MRI-image volumes.
+
+`Usage`
+1. Load an input volume.  
+2. (Optional) Adjust parameters such as thresholds, aspect ratio, or smoothing iterations.  
+3. (Optional) Enable *Extract Centerline* to generate a markups curve.  
+4. Click 'Apply' to run the full pipeline.  
+
+""")
+    
+        # TODO: replace with organization, grant and thanks
+        self.parent.acknowledgementText = _("""
+The module builds on the 3D Slicer Scripted Loadable Module template. 
+""")
+
+        # Additional initialization step after application startup is complete
+        slicer.app.connect("startupCompleted()", registerSampleData)
+
+
+#
+# Register sample data sets in Sample Data module
+#
+
+
+def registerSampleData():
+    """Add data sets to Sample Data module."""
+    # It is always recommended to provide sample data for users to make it easy to try the module,
+    # but if no sample data is available then this method (and associated startupCompeted signal connection) can be removed.
+
+    import SampleData
+
+    iconsPath = os.path.join(os.path.dirname(__file__), "Resources/Icons")
+
+    # To ensure that the source code repository remains small (can be downloaded and installed quickly)
+    # it is recommended to store data sets that are larger than a few MB in a Github release.
+
+    # DBS1
+    SampleData.SampleDataLogic.registerCustomSampleDataSource(
+        # Category and sample name displayed in Sample Data module
+        category="DBS",
+        sampleName="DBS1",
+        # Thumbnail should have size of approximately 260x280 pixels and stored in Resources/Icons folder.
+        # It can be created by Screen Capture module, "Capture all views" option enabled, "Number of images" set to "Single".
+        thumbnailFileName=os.path.join(iconsPath, "DBS1.png"),
+        # Download URL and target file name
+        uris="https://github.com/Slicer/SlicerTestingData/releases/download/SHA256/998cb522173839c78657f4bc0ea907cea09fd04e44601f17c82ea27927937b95",
+        fileNames="DBS1.nrrd",
+        # Checksum to ensure file integrity. Can be computed by this command:
+        #  import hashlib; print(hashlib.sha256(open(filename, "rb").read()).hexdigest())
+        checksums="SHA256:998cb522173839c78657f4bc0ea907cea09fd04e44601f17c82ea27927937b95",
+        # This node name will be used when the data set is loaded
+        nodeNames="DBS1",
+    )
+
+    # DBS2
+    SampleData.SampleDataLogic.registerCustomSampleDataSource(
+        # Category and sample name displayed in Sample Data module
+        category="DBS",
+        sampleName="DBS2",
+        thumbnailFileName=os.path.join(iconsPath, "DBS2.png"),
+        # Download URL and target file name
+        uris="https://github.com/Slicer/SlicerTestingData/releases/download/SHA256/1a64f3f422eb3d1c9b093d1a18da354b13bcf307907c66317e2463ee530b7a97",
+        fileNames="DBS2.nrrd",
+        checksums="SHA256:1a64f3f422eb3d1c9b093d1a18da354b13bcf307907c66317e2463ee530b7a97",
+        # This node name will be used when the data set is loaded
+        nodeNames="DBS2",
+    )
+
+
+#
+# DBSParameterNode
+#
+
+
+@parameterNodeWrapper
+class DBSParameterNode:
+    """
+    The parameters needed by module.
+
+    inputVolume - The volume to segment
+    segmentName - The default segmentation name
+    doCenterline - Optional choice of generating centerline
+    """
+
+    inputVolume: vtkMRMLScalarVolumeNode
+    segmentName: str = "Segment_1"
+    doCenterline: bool = False
+    
+
+#
+# DBSWidget
+#
+
+
+class DBSWidget(ScriptedLoadableModuleWidget, VTKObservationMixin):
+    """Uses ScriptedLoadableModuleWidget base class, available at:
+    https://github.com/Slicer/Slicer/blob/main/Base/Python/slicer/ScriptedLoadableModule.py
+    """
+
+    def __init__(self, parent=None) -> None:
+        """Called when the user opens the module the first time and the widget is initialized."""
+        ScriptedLoadableModuleWidget.__init__(self, parent)
+        VTKObservationMixin.__init__(self)  # needed for parameter node observation
+        self.logic = None
+        self._parameterNode = None
+        self._parameterNodeGuiTag = None
+
+    def setup(self) -> None:
+        """Called when the user opens the module the first time and the widget is initialized."""
+        ScriptedLoadableModuleWidget.setup(self)
+
+        # Load widget from .ui file (created by Qt Designer).
+        # Additional widgets can be instantiated manually and added to self.layout.
+        uiWidget = slicer.util.loadUI(self.resourcePath("UI/DBS.ui"))
+        self.layout.addWidget(uiWidget)
+        self.ui = slicer.util.childWidgetVariables(uiWidget)
+
+        # Set scene in MRML widgets. Make sure that in Qt designer the top-level qMRMLWidget's
+        # "mrmlSceneChanged(vtkMRMLScene*)" signal in is connected to each MRML widget's.
+        # "setMRMLScene(vtkMRMLScene*)" slot.
+        uiWidget.setMRMLScene(slicer.mrmlScene)
+
+        # Create logic class. Logic implements all computations that should be possible to run
+        # in batch mode, without a graphical user interface.
+        self.logic = DBSLogic()
+
+        # Connections
+
+        # These connections ensure that we update parameter node when scene is closed
+        self.addObserver(slicer.mrmlScene, slicer.mrmlScene.StartCloseEvent, self.onSceneStartClose)
+        self.addObserver(slicer.mrmlScene, slicer.mrmlScene.EndCloseEvent, self.onSceneEndClose)
+
+        # Buttons
+        self.ui.applyButton.connect("clicked(bool)", self.onApplyButton)
+
+        # Make sure parameter node is initialized (needed for module reload)
+        self.initializeParameterNode()
+
+    def cleanup(self) -> None:
+        """Called when the application closes and the module widget is destroyed."""
+        self.removeObservers()
+
+    def enter(self) -> None:
+        """Called each time the user opens this module."""
+        # Make sure parameter node exists and observed
+        self.initializeParameterNode()
+
+    def exit(self) -> None:
+        """Called each time the user opens a different module."""
+        # Do not react to parameter node changes (GUI will be updated when the user enters into the module)
+        if self._parameterNode:
+            self._parameterNode.disconnectGui(self._parameterNodeGuiTag)
+            self._parameterNodeGuiTag = None
+            self.removeObserver(self._parameterNode, vtk.vtkCommand.ModifiedEvent, self._checkCanApply)
+
+    def onSceneStartClose(self, caller, event) -> None:
+        """Called just before the scene is closed."""
+        # Parameter node will be reset, do not use it anymore
+        self.setParameterNode(None)
+
+    def onSceneEndClose(self, caller, event) -> None:
+        """Called just after the scene is closed."""
+        # If this module is shown while the scene is closed then recreate a new parameter node immediately
+        if self.parent.isEntered:
+            self.initializeParameterNode()
+
+    def initializeParameterNode(self) -> None:
+        """Ensure parameter node exists and observed."""
+        # Parameter node stores all user choices in parameter values, node selections, etc.
+        # so that when the scene is saved and reloaded, these settings are restored.
+
+        self.setParameterNode(self.logic.getParameterNode())
+
+        # Select default input nodes if nothing is selected yet to save a few clicks for the user
+        if not self._parameterNode.inputVolume:
+            firstVolumeNode = slicer.mrmlScene.GetFirstNodeByClass("vtkMRMLScalarVolumeNode")
+            if firstVolumeNode:
+                self._parameterNode.inputVolume = firstVolumeNode
+
+    def setParameterNode(self, inputParameterNode: Optional[DBSParameterNode]) -> None:
+        """
+        Set and observe parameter node.
+        Observation is needed because when the parameter node is changed then the GUI must be updated immediately.
+        """
+
+        if self._parameterNode:
+            self._parameterNode.disconnectGui(self._parameterNodeGuiTag)
+            self.removeObserver(self._parameterNode, vtk.vtkCommand.ModifiedEvent, self._checkCanApply)
+        self._parameterNode = inputParameterNode
+        if self._parameterNode:
+            # Note: in the .ui file, a Qt dynamic property called "SlicerParameterName" is set on each
+            # ui element that needs connection.
+            self._parameterNodeGuiTag = self._parameterNode.connectGui(self.ui)
+            self.addObserver(self._parameterNode, vtk.vtkCommand.ModifiedEvent, self._checkCanApply)
+            self._checkCanApply()
+
+
+    def _checkCanApply(self, caller=None, event=None) -> None:
+        if self._parameterNode and self._parameterNode.inputVolume:
+            self.ui.applyButton.toolTip = _("Run the automatic segmentation")
+            self.ui.applyButton.enabled = True
+        else:
+            self.ui.applyButton.toolTip = _("Select input volume")
+            self.ui.applyButton.enabled = False
+
+
+    def onApplyButton(self) -> None:
+        with slicer.util.tryWithErrorDisplay(_("Failed to run pipeline."), waitCursor=True):
+            p = self._parameterNode  # already synced via connectGui
+            # Fallback: if you also added a file path input, load it here before using p.inputVolume
+            self.logic.process(
+                inputVolume=p.inputVolume,
+                segment_name=p.segmentName,
+                do_centerline=p.doCenterline
+            )
+
+#
+# DBSLogic
+#
+
+
+class DBSLogic(ScriptedLoadableModuleLogic):
+    """This class should implement all the actual
+    computation done by your module.  The interface
+    should be such that other python code can import
+    this class and make use of the functionality without
+    requiring an instance of the Widget.
+    Uses ScriptedLoadableModuleLogic base class, available at:
+    https://github.com/Slicer/Slicer/blob/main/Base/Python/slicer/ScriptedLoadableModule.py
+    """
+
+    def getParameterNode(self):
+        return DBSParameterNode(super().getParameterNode())
+
+
+    def process(self,
+                inputVolume: vtkMRMLScalarVolumeNode,
+                segment_name: str = "Segment_1",
+                do_centerline: bool = False):
+        if not inputVolume:
+            raise ValueError("Input volume is invalid")
+
+        import importlib, logging, time
+        start = time.time()
+        logging.info("Pipeline started")
+
+        # import your helper from DBS/Lib/dbs_segmentation.py
+        from Lib import dbs_segmentation
+        importlib.reload(dbs_segmentation)  # handy during development
+        dbs_segmentation.run_seg_pipeline(
+            inputVolume,
+            do_centerline=do_centerline
+        )
+        logging.info(f"Pipeline finished in {time.time()-start:.2f}s")
+
+#
+# DBSTest
+#
+
+
+class DBSTest(ScriptedLoadableModuleTest):
+    """
+    This is the test case for your scripted module.
+    Uses ScriptedLoadableModuleTest base class, available at:
+    https://github.com/Slicer/Slicer/blob/main/Base/Python/slicer/ScriptedLoadableModule.py
+    """
+
+    def setUp(self):
+        """Do whatever is needed to reset the state - typically a scene clear will be enough."""
+        slicer.mrmlScene.Clear()
+
+    def runTest(self):
+        """Run as few or as many tests as needed here."""
+        self.setUp()
+        self.test_DBS1()
+
+    def test_DBS1(self):
+        """Ideally you should have several levels of tests.  At the lowest level
+        tests should exercise the functionality of the logic with different inputs
+        (both valid and invalid).  At higher levels your tests should emulate the
+        way the user would interact with your code and confirm that it still works
+        the way you intended.
+        One of the most important features of the tests is that it should alert other
+        developers when their changes will have an impact on the behavior of your
+        module.  For example, if a developer removes a feature that you depend on,
+        your test should break so they know that the feature is needed.
+        """
+
+        self.delayDisplay("Starting the test")
+
+        # Get/create input data
+
+        import SampleData
+
+        registerSampleData()
+        inputVolume = SampleData.downloadSample("DBS1")
+        self.delayDisplay("Loaded test data set")
+
+        inputScalarRange = inputVolume.GetImageData().GetScalarRange()
+        self.assertEqual(inputScalarRange[0], 0)
+        self.assertEqual(inputScalarRange[1], 695)
+
+        outputVolume = slicer.mrmlScene.AddNewNodeByClass("vtkMRMLScalarVolumeNode")
+        threshold = 100
+
+        # Test the module logic
+
+        logic = DBSLogic()
+
+        # Test algorithm with non-inverted threshold
+        logic.process(inputVolume, outputVolume, threshold, True)
+        outputScalarRange = outputVolume.GetImageData().GetScalarRange()
+        self.assertEqual(outputScalarRange[0], inputScalarRange[0])
+        self.assertEqual(outputScalarRange[1], threshold)
+
+        # Test algorithm with inverted threshold
+        logic.process(inputVolume, outputVolume, threshold, False)
+        outputScalarRange = outputVolume.GetImageData().GetScalarRange()
+        self.assertEqual(outputScalarRange[0], inputScalarRange[0])
+        self.assertEqual(outputScalarRange[1], inputScalarRange[1])
+
+        self.delayDisplay("Test passed")
---
+++ DBS/Lib/dbs_segmentation.py
@@ -0,0 +1,214 @@
+import slicer, vtk
+import numpy as np
+from scipy.ndimage import label as cc_label, find_objects as cc_find_objects, binary_closing
+
+def _get_or_create_segmentation(volumeNode, baseName="Segmentation"):
+    """Return a segmentation node named `baseName` if present; otherwise create it.
+    Always ensures reference geometry and display nodes exist."""
+    try:
+        segNode = slicer.util.getNode(baseName)
+        if segNode.GetClassName() != "vtkMRMLSegmentationNode":
+            # If a different node has that name, make a new proper one
+            raise slicer.util.MRMLNodeNotFoundException("")
+    except slicer.util.MRMLNodeNotFoundException:
+        segNode = slicer.mrmlScene.AddNewNodeByClass("vtkMRMLSegmentationNode", baseName)
+    segNode.CreateDefaultDisplayNodes()
+    segNode.SetReferenceImageGeometryParameterFromVolumeNode(volumeNode)
+    return segNode
+
+def _ensure_target_segment(segmentationNode, preferredName="tube"):
+    """Return a segmentId to work with:
+       - if there are segments, use the last one (latest added)
+       - otherwise create an empty segment (named `preferredName`) and use it
+    """
+    seg = segmentationNode.GetSegmentation()
+    n = seg.GetNumberOfSegments()
+    if n == 0:
+        # Create one and grab its id
+        seg.AddEmptySegment(preferredName)
+        segmentId = seg.GetNthSegmentID(seg.GetNumberOfSegments() - 1)
+    else:
+        # Use the most recently added segment
+        segmentId = seg.GetNthSegmentID(n - 1)
+    return segmentId
+
+def _numpy_from_segment(segmentationNode, segmentId, referenceVolumeNode):
+    """Export the given segment to a temporary LabelMapVolume and return a NumPy mask [Z,Y,X]."""
+    # Make sure binary labelmap representation exists
+    binaryName = slicer.vtkSegmentationConverter.GetBinaryLabelmapRepresentationName()
+    segmentationNode.GetSegmentation().CreateRepresentation(binaryName)
+
+    # Export to a labelmap aligned with the volume geometry
+    labelmapNode = slicer.mrmlScene.AddNewNodeByClass("vtkMRMLLabelMapVolumeNode")
+    slicer.modules.segmentations.logic().ExportSegmentsToLabelmapNode(
+        segmentationNode, [segmentId], labelmapNode, referenceVolumeNode
+    )
+    arr = slicer.util.arrayFromVolume(labelmapNode)  # [Z,Y,X]
+    slicer.mrmlScene.RemoveNode(labelmapNode)
+    if arr is None:
+        # Guard against empty/failed export
+        arr = np.zeros(slicer.util.arrayFromVolume(referenceVolumeNode).shape, dtype=np.uint8)
+    return (arr > 0).astype(np.uint8)
+
+def run_seg_pipeline(path_or_volumeNode, *, do_centerline=False):
+    """
+    Fully automatic pipeline. Reuses the last segmentation & last segment so you never
+    depend on a hard-coded name, and multiple Apply clicks work cleanly.
+    """
+    # ---------------------------
+    # INPUT HANDLING
+    # ---------------------------
+    if isinstance(path_or_volumeNode, str):
+        volumeNode = slicer.util.loadVolume(path_or_volumeNode)
+        if volumeNode is None:
+            raise RuntimeError(f"Failed to load volume from: {path_or_volumeNode}")
+    else:
+        volumeNode = path_or_volumeNode
+        if volumeNode is None:
+            raise ValueError("No input volume node provided.")
+
+    # ============================================================
+    # ================ PART 1 (threshold segmentation) ===========
+    # ============================================================
+    segmentationNode = _get_or_create_segmentation(volumeNode, baseName="Segmentation")
+    segmentId = _ensure_target_segment(segmentationNode, preferredName="tube")
+
+    # Segment Editor: Threshold into *that* segment
+    segmentEditorWidget = slicer.qMRMLSegmentEditorWidget()
+    segmentEditorWidget.setMRMLScene(slicer.mrmlScene)
+    segmentEditorNode = slicer.mrmlScene.AddNewNodeByClass("vtkMRMLSegmentEditorNode")
+    segmentEditorWidget.setMRMLSegmentEditorNode(segmentEditorNode)
+    segmentEditorWidget.setSegmentationNode(segmentationNode)
+    segmentEditorWidget.setSourceVolumeNode(volumeNode)
+    segmentEditorWidget.setCurrentSegmentID(segmentId)
+
+    segmentEditorWidget.setActiveEffectByName("Threshold")
+    effect = segmentEditorWidget.activeEffect()
+    effect.setParameter("MinimumThreshold", "40")
+    effect.setParameter("MaximumThreshold", "95")
+    effect.self().onApply()
+
+    # Clean up editor widget/node
+    segmentEditorWidget = None
+    slicer.mrmlScene.RemoveNode(segmentEditorNode)
+
+    # Optional: show surface
+    segmentationNode.CreateClosedSurfaceRepresentation()
+
+    # ============================================================
+    # ============ PART 2 (remove unnecessary parts) =============
+    # ============================================================
+    # Robust read of the segment mask as NumPy
+    seg_np = _numpy_from_segment(segmentationNode, segmentId, volumeNode)
+
+    labeled_array, _ = cc_label(seg_np)
+    object_slices = cc_find_objects(labeled_array)
+
+    ASPECT_RATIO_THRESHOLD = 3.5
+    tube_labels = []
+    for i, object_slice in enumerate(object_slices):
+        if object_slice is None:
+            continue
+        z_slice, y_slice, x_slice = object_slice
+        height = y_slice.stop - y_slice.start
+        width  = x_slice.stop - x_slice.start
+        aspect_ratio = (height / width) if width > 0 else height * 10
+        if aspect_ratio > ASPECT_RATIO_THRESHOLD:
+            tube_labels.append(i + 1)
+
+    final_tube_mask = np.isin(labeled_array, tube_labels).astype(np.uint8)
+
+    slicer.util.updateSegmentBinaryLabelmapFromArray(
+        final_tube_mask, segmentationNode, segmentId, volumeNode
+    )
+    segmentationNode.CreateClosedSurfaceRepresentation()
+
+    # ============================================================
+    # =============== PART 3 (smoothing + bbox fill)  ============
+    # ============================================================
+    print("Smoothing the segmentation...")
+    smoothed_mask = binary_closing(final_tube_mask.astype(bool), iterations=4)
+
+    print("Updating the segment with the smoothed version...")
+    slicer.util.updateSegmentBinaryLabelmapFromArray(
+        smoothed_mask.astype("uint8"),
+        segmentationNode, segmentId, volumeNode
+    )
+    segmentationNode.CreateClosedSurfaceRepresentation()
+    print("Segmentation successfully updated with the smoothed version!")
+
+    # bounding boxes (sagittal + coronal)
+    final_mask = np.zeros_like(smoothed_mask, dtype=bool)
+
+    # Z-planes
+    for z in range(smoothed_mask.shape[0]):
+        current_slice = smoothed_mask[z, :, :]
+        coords = np.argwhere(current_slice)
+        if coords.size:
+            y_min, x_min = coords.min(axis=0)
+            y_max, x_max = coords.max(axis=0)
+            final_mask[z, y_min:y_max+1, x_min:x_max+1] = True
+
+    # X-columns
+    for x in range(smoothed_mask.shape[2]):
+        current_slice = smoothed_mask[:, :, x]
+        coords = np.argwhere(current_slice)
+        if coords.size:
+            z_min, y_min = coords.min(axis=0)
+            z_max, y_max = coords.max(axis=0)
+            final_mask[z_min:z_max+1, y_min:y_max+1, x] = True
+
+    print("Updating the segment with the bounding box result...")
+    slicer.util.updateSegmentBinaryLabelmapFromArray(
+        final_mask.astype("uint8"),
+        segmentationNode, segmentId, volumeNode
+    )
+    segmentationNode.CreateClosedSurfaceRepresentation()
+    print("Segmentation successfully updated")
+
+    results = {
+        "volumeNode": volumeNode,
+        "segmentationNode": segmentationNode,
+        "segmentId": segmentId,          # <-- use this id everywhere; no names needed
+        "final_mask": final_mask,
+        "smoothed_mask": smoothed_mask,
+    }
+
+    # ============================================================
+    # =============== PART 4 (OPTIONAL centerline) ===============
+    # ============================================================
+    if do_centerline:
+        print("Calculating centerline directly from slice midpoints...")
+        centerline_points = []
+        for y in range(smoothed_mask.shape[1]):
+            current_slice = final_mask[:, y, :]
+            coords = np.argwhere(current_slice)
+            if coords.size:
+                mid_z, mid_x = coords.mean(axis=0)
+                centerline_points.append((mid_z, y, mid_x))
+
+        ijkToRasMatrix = vtk.vtkMatrix4x4()
+        volumeNode.GetIJKToRASMatrix(ijkToRasMatrix)
+
+        centerline_points_ras = []
+        for ijk_point in centerline_points:
+            p_ijk = [ijk_point[2], ijk_point[1], ijk_point[0], 1]  # I,J,K -> X,Y,Z
+            p_ras = [0, 0, 0, 0]
+            ijkToRasMatrix.MultiplyPoint(p_ijk, p_ras)
+            centerline_points_ras.append(p_ras[0:3])
+
+        curveNode = slicer.mrmlScene.AddNewNodeByClass("vtkMRMLMarkupsCurveNode")
+        curveNode.SetName("CenterLine")
+        for point in centerline_points_ras:
+            curveNode.AddControlPoint(vtk.vtkVector3d(point[0], point[1], point[2]))
+        dn = curveNode.GetDisplayNode()
+        if dn:
+            dn.SetColor(1, 0, 1)
+            dn.SetGlyphScale(1.0)
+            dn.SetLineWidth(1.0)
+
+        print(f"Successfully created a line with {len(centerline_points_ras)} points.")
+        results["curveNode"] = curveNode
+
+    return results
+
